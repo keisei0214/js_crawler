9805379e8318ef189ed7b20712e7e60b68726e70
DESKTOP-M0OOP2T\Keisuke Isemoto <5ek2u0i1ek9@gmail.com>
2019-04-05 23:04:10 +0900

    関数を呼び出すように変更

--- sample/neco_video_crawler.js
+++ sample/neco_video_crawler.js
@@ -4,54 +4,13 @@
 // @author       keisei_0214
 // @match        https://www.google.co.jp/search?q=%E7%8C%AB&tbm=vid&start=*
 // @grant        none
+// @require      https://raw.githubusercontent.com/keisei0214/js_crawler/master/sessionstrage-control.js
 // ==/UserScript==
 
 // sessionStorageの初期化
 if(sessionStorage.getItem('size') == null){
     sessionStorage.setItem('size', '0');
 }
-
-/**
- * sessionStorageにデータを追加する関数。このデータが出力時の1列にあたる。複数要素を追加する場合、何らかの区切りを入れて、結合したstrにする
- * @param  {str} data 追加するデータ。
- */
-function addData(data){
-    sessionStorage.setItem(parseInt(sessionStorage.getItem('size')), data)
-    sessionStorage.setItem('size', String(parseInt(sessionStorage.getItem('size'))+1))
-}
-
-/**
- * 全データをcsvの形式の文字列で返す
- * @return   {str} 出力データ
- */
-function getAllData(){
-    let ret = ''
-    for(let i = 0; i < parseInt(sessionStorage.getItem('size')); i++){
-        ret += sessionStorage.getItem(i) + '\n'
-    }
-    return ret
-}
-
-/**
- * 何らかのキーが押されたときの処理。result.csvというファイル名でダウンロードする
- *
- */
-function handleKeydown(event){
-    // utf8
-    const bom = '\uFEFF';
-
-    var blob = new Blob([bom, getAllData() ], { "type" : "text/csv" });
-
-    var a = document.createElement("a");
-    a.href = (window.URL || window.webkitURL).createObjectURL(blob);;
-    a.target = '_blank';
-    a.download = 'result.csv';
-
-    a.click();
-}
-
-// イベントリスナーに追加
-//window.addEventListener("keydown", handleKeydown);
 
 (async() => {
     let href = location.href.match(/(https:\/\/www.google\.co\.jp\/search\?q=%E7%8C%AB&tbm=vid&start=)(\d+)/)
