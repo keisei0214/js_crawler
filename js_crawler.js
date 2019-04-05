221c4a6b98942dbecd5ac37f47b8bd67a43f699c
DESKTOP-M0OOP2T\Keisuke Isemoto <5ek2u0i1ek9@gmail.com>
2019-04-05 22:57:57 +0900

    関数以外を削除

--- js_crawler.js
+++ js_crawler.js
@@ -1,16 +1,3 @@
-// ==UserScript==
-// @name         js_crawler
-// @version      0.1
-// @author       keisei_0214
-// @match        [対象URL]
-// @grant        none
-// ==/UserScript==
-
-// sessionStorageの初期化
-if(sessionStorage.getItem('size') == null){
-    sessionStorage.setItem('size', '0');
-}
-
 /**
  * sessionStorageにデータを追加する関数。このデータが出力時の1列にあたる。複数要素を追加する場合、何らかの区切りを入れて、結合したstrにする
  * @param  {str} data 追加するデータ。
@@ -49,23 +36,3 @@ function handleKeydown(event){
 
     a.click();
 }
-
-// イベントリスナーに追加
-window.addEventListener("keydown", handleKeydown);
-
-(async() => {
-    // 引数の秒数分待つ
-    const wait = ( /** @type {number} */ second) => new Promise((resolve) => setTimeout(() => resolve(), second * 1000))
-    await wait(1)
-
-    // 取得するデータの設定などを行う
-    let text = document.querySelector("body").innerText
-
-    // 取得したデータを追加する
-    addData(text)
-
-    // 次のページに移動する
-    location.href = 'http://hogehoge'
-
-    }
-)();
